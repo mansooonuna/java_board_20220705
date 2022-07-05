6879a6c123d19d6defca697bcc6d677795a6d651
mansooonuna <parkdasom95@gmail.com>
2022-07-05 21:50:33 +0900

    종료버튼

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
     System.out.println("== 게시판 v 0.1 ==");
     System.out.println("== 프로그램 시작 ==");
-    System.out.println("명령)");
+
+
+    while (true) {
+      System.out.print("명령) ");
+      String input = sc.nextLine();
+      System.out.println("입력된 명령어 : " + input);
+
+      if (input.equals("exit")) {
+        break;
+      }
+
+    }
+
     System.out.println("== 프로그램 종료 ==");
 
-
-
+    sc.close();
 
   }
 }
